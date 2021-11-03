5ae3246f6cd06a169c26f41c7db815f45fbe33c1
Brad <kairess87@gmail.com>
2021-11-03 17:50:03 +0900

    initial commit

---
+++ client.py
@@ -0,0 +1,28 @@
+import airshare
+import requests
+import socket
+import webbrowser
+
+SERVER_ADDR = 'http://172.30.1.13:32456'
+
+CODE = 'brad-macbook-pro'
+PORT = 23456
+
+local_ip_addr = socket.inet_ntoa(airshare.utils.get_local_ip_address())
+
+data = {
+    'code': CODE,
+    'ip_addr': local_ip_addr,
+    'port': PORT,
+}
+
+r = requests.post('%s/register' % (SERVER_ADDR,), data)
+
+print(r.json())
+
+try:
+    webbrowser.open('%s/browse' % SERVER_ADDR)
+except:
+    print('%s/browse' % SERVER_ADDR)
+
+airshare.receiver.receive_server(code=CODE, port=PORT)
---
+++ server.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template, jsonify
+import time, json, os
+
+DB_PATH = 'db.json'
+
+if not os.path.exists(DB_PATH):
+    with open(DB_PATH, 'w') as f:
+        json.dump({}, f)
+
+app = Flask(__name__)
+
+@app.template_filter('ctime')
+def timectime(s):
+    return time.ctime(s)
+
+@app.route('/browse')
+def browse():
+    with open(DB_PATH, 'r') as f:
+        db = json.load(f)
+
+    return render_template('browse.html', db=db)
+
+@app.route('/register', methods=['POST'])
+def register():
+    client_code = str(request.form['code']).lower()
+    client_ip_addr = request.form['ip_addr']
+    client_port = request.form['port']
+    updated_at = time.time()
+
+    with open(DB_PATH, 'r') as f:
+        db = json.load(f)
+
+    if client_ip_addr not in db:
+        db[client_ip_addr] = {
+            'code': client_code,
+            'ip_addr': client_ip_addr,
+            'port': client_port,
+        }
+
+    db[client_ip_addr]['updated_at'] = updated_at
+
+    with open(DB_PATH, 'w') as f:
+        json.dump(db, f)
+
+    return jsonify({
+        'result': True,
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=32456, debug=True)
